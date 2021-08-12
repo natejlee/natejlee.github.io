db2be612cd7e79904ad7bdd6ba697c2ec2b97961
natejlee <nathaniel.lee@modernmsg.com>
2021-08-12 01:58:44 +0000

    Deploying to gh-pages from @ natejlee/natelee.github.io@412eb4f86b26fd692183c322ba8e2b5953ec74ca 🚀

---
+++ _next/static/z90e_fiVActCD6ffJQ0eh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6fceadb801d1dda4d4ca.css","static/chunks/pages/index-92b152cfb1006d745996.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
